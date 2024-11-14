fdc5a726ac6e241c7a849e8a49b0208cb56edff7
Thaily <lyvinhthai321@gmail.com>
2024-11-14 22:40:22 +0700

    update

--- middlewares/client/auth.js
+++ middlewares/client/auth.js
@@ -4,19 +4,26 @@ require('dotenv').config();
 const secret = process.env.JWT_SECRET
 
 module.exports.requireAuth = async (req, res, next) => {
-  const token = req.cookies.token 
-  if(!token) {
-    return;
+  // Lấy token từ header Authorization
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({
+      "code": "error",
+      "msg": "Token không được cung cấp"
+    });
   }
+
   jwt.verify(token, secret, async (err, decoded) => {
-    if(err){
-      res.json({
+    if (err) {
+      return res.status(403).json({
         "code": "error",
-        "msg": "token khong hop le"
-      })
-    }else{
-      res.locals.account = decoded.accountToken
-      next()
+        "msg": "Token không hợp lệ"
+      });
+    } else {
+      res.locals.account = decoded.accountToken;
+      next();
     }
-  })
+  });
 }
